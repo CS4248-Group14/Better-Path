5c1ba8f3ef0d12e3f4fb2cbaef7ed8f0692a333e
T-Fang <tianfangsg@gmail.com>
2022-04-03 22:02:54 +0800

    Add comments to Dataset methods

--- code/dataset.py
+++ code/dataset.py
@@ -1,5 +1,3 @@
-from __future__ import division
-
 import pickle, random
 import numpy as np
 from itertools import cycle
@@ -110,12 +108,18 @@ class Dataset:
         return [all_feature_lengths[f] for f in self.feature_names]
 
     def get_v_fea_len(self):
+        """
+        Get vertex feature lengths corresponding to the input feature_names specified during initialization
+        """
         return [
             all_feature_lengths[f] for f in self.feature_names
             if f.startswith('v')
         ]
 
     def get_e_fea_len(self):
+        """
+        Get edge feature lengths corresponding to the input feature_names specified during initialization
+        """
         return [
             all_feature_lengths[f] for f in self.feature_names
             if f.startswith('e')
@@ -125,6 +129,12 @@ class Dataset:
         return len(self.get_features(id)[0])
 
     def get_features(self, id):
+        """
+        Get features and heuristics of the path with the given id
+        
+        :param id: id of the path
+        :return: a tuple containing (vertex features, edge features, heuristics)
+        """
         v_features = []
         e_features = []
         for f in self.feature_names:
@@ -383,109 +393,3 @@ class Dataset:
                     heuristics[i] = heuristics[i].cuda()
         return ((v_features_A, e_features_A), heuristics_A,
                 (v_features_B, e_features_B), heuristics_B)
-
-
-class HeuristicPreprocessor:
-    def __init__(self, name, src_data_path, tgt_dir_path):
-        self.name = name
-        sampled_problems = pickle.load(open('%s/paths.pkl' % src_data_path,
-                                            'rb'),
-                                       encoding='latin1')
-        self.texts = dict()
-        print('loading problem plain texts')
-        for id_num in sampled_problems:
-            f_short = sampled_problems[id_num]['forward']['short']
-            r_short = sampled_problems[id_num]['reverse']['short']
-            self.texts[id_num + 'f'] = f_short
-            self.texts[id_num + 'r'] = r_short
-        self.tgt_dir_path = tgt_dir_path
-
-    def _eval_path(self, id_):
-        raise NotImplementedError()
-
-    def calc_heuristics(self):
-        scores = dict()
-        for id_ in self.texts:
-            scores[id_] = self._eval_path(id_)
-        with open('%s/%s.pkl' % (self.tgt_dir_path, self.name), 'wb') as file:
-            pickle.dump(scores, file)
-
-
-class STHeuristicsExtractor(HeuristicPreprocessor):
-    def __init__(self, src_data_path, tgt_dir_path, v_enc_path):
-        super(STHeuristicsExtractor, self).__init__('st', src_data_path,
-                                                    tgt_dir_path)
-        with open(v_enc_path, 'rb') as file:
-            self.v_emb = pickle.load(file, encoding='latin1')
-        self.cos = torch.nn.CosineSimilarity(dim=0)
-
-    def _eval_path(self, id_):
-        emb_data = self.v_emb[id_]
-        score = self.cos(torch.tensor(emb_data[0]),
-                         torch.tensor(emb_data[len(emb_data) - 1]))
-        return score.item()
-
-
-class PairwiseHeuristicsExtractor(HeuristicPreprocessor):
-    def __init__(self, src_data_path, tgt_dir_path, v_enc_path):
-        super(PairwiseHeuristicsExtractor,
-              self).__init__('pairwise', src_data_path, tgt_dir_path)
-        with open(v_enc_path, 'rb') as file:
-            self.v_emb = pickle.load(file, encoding='latin1')
-        self.cos = torch.nn.CosineSimilarity(dim=0)
-
-    def _eval_path(self, id_):
-        emb_data = self.v_emb[id_]
-        size = len(emb_data)
-        total_score = 0
-        for i in range(size - 1):
-            total_score += self.cos(torch.tensor(emb_data[i]),
-                                    torch.tensor(emb_data[i + 1])).item()
-        return total_score / (size + 1)
-
-
-class RFHeuristicsExtractor(HeuristicPreprocessor):
-    # simplified: product of degrees (for easier standardization, take log)
-    def __init__(self, src_data_path, tgt_dir_path, v_deg_path):
-        super(RFHeuristicsExtractor, self).__init__('rf', src_data_path,
-                                                    tgt_dir_path)
-        with open(v_deg_path, 'rb') as file:
-            self.v_deg = pickle.load(file, encoding='latin1')
-
-    def _eval_path(self, id_):
-        score = 0
-        deg = self.v_deg[id_]
-        for d in deg:
-            score += np.log(d[0])
-        return score
-
-
-class LengthHeuristicsExtractor(HeuristicPreprocessor):
-    def __init__(self, src_data_path, tgt_dir_path):
-        super(LengthHeuristicsExtractor,
-              self).__init__('length', src_data_path, tgt_dir_path)
-
-    def _eval_path(self, id_):
-        path = self.texts[id_]
-        tokens = path.split()
-        score = len(tokens) // 2 + 1
-        return score
-
-
-if __name__ == '__main__':
-    # prepare heuristics
-    sthe = STHeuristicsExtractor(
-        '../data/science', '../prepare_data/heuristics',
-        '../prepare_data/features/v_enc_embedding.pkl')
-    sthe.calc_heuristics()
-    phe = PairwiseHeuristicsExtractor(
-        '../data/science', '../prepare_data/heuristics',
-        '../prepare_data/features/v_enc_embedding.pkl')
-    phe.calc_heuristics()
-    rfhe = RFHeuristicsExtractor('../data/science',
-                                 '../prepare_data/heuristics',
-                                 '../prepare_data/features/v_deg.pkl')
-    rfhe.calc_heuristics()
-    lhe = LengthHeuristicsExtractor('../data/science',
-                                    '../prepare_data/heuristics')
-    lhe.calc_heuristics()
--- code/learn.py
+++ code/learn.py
@@ -8,15 +8,20 @@ from model import ChainEncoder, Predictor, AlternateChainEncoder, ConcatChainEnc
 from dataset import Dataset
 from multiprocessing import Pool
 
-def train(dataset, features, heuristics, fea_len, split_frac, out_file, use_gpu, max_iter, batch_size, ckpt_path):
+
+def train(dataset, features, heuristics, fea_len, split_frac, out_file,
+          use_gpu, max_iter, batch_size, ckpt_path):
     if isinstance(out_file, str):
         out_file = open(out_file, 'w')
-    d = Dataset(features, heuristics, split_frac, use_gpu, '../prepare_data/features', '../prepare_data/heuristics', f'../data/{dataset}')
+    d = Dataset(features, heuristics, split_frac, use_gpu,
+                '../prepare_data/features', '../prepare_data/heuristics',
+                f'../data/{dataset}')
     print('defining architecture')
     # enc = ChainEncoder(d.get_v_fea_len(), d.get_e_fea_len(), fea_len, 'last')
-    enc = AlternateChainEncoder(d.get_v_fea_len(), d.get_e_fea_len(), fea_len, 'last')
+    enc = AlternateChainEncoder(d.get_v_fea_len(), d.get_e_fea_len(), fea_len,
+                                'last')
     # enc = ConcatChainEncoder_Brendan(d.get_v_fea_len(), d.get_e_fea_len(), fea_len, 'last')
-    
+
     predictor = Predictor(fea_len + len(heuristics))
     loss = nn.NLLLoss()
     if use_gpu:
@@ -24,13 +29,15 @@ def train(dataset, features, heuristics, fea_len, split_frac, out_file, use_gpu,
         predictor.cuda()
         loss.cuda()
 
-    optimizer = optim.Adam(list(enc.parameters())+list(predictor.parameters()))
+    optimizer = optim.Adam(
+        list(enc.parameters()) + list(predictor.parameters()))
 
     print('training')
     test_chain_A, test_h_A, test_chain_B, test_h_B, test_y = d.get_test_pairs()
     test_y = test_y.data.cpu().numpy()
     for train_iter in range(max_iter):
-        chains_A, heuristic_A, chains_B, heuristic_B, y = d.get_train_pairs(batch_size)
+        chains_A, heuristic_A, chains_B, heuristic_B, y = d.get_train_pairs(
+            batch_size)
         enc.zero_grad()
         predictor.zero_grad()
         output_A = enc(chains_A)
@@ -48,25 +55,30 @@ def train(dataset, features, heuristics, fea_len, split_frac, out_file, use_gpu,
         output_test_B = enc(test_chain_B)
         output_test_A = torch.concat((output_test_A, *test_h_A), dim=1)
         output_test_B = torch.concat((output_test_B, *test_h_B), dim=1)
-        softmax_output = predictor(output_test_A, output_test_B).data.cpu().numpy()
+        softmax_output = predictor(output_test_A,
+                                   output_test_B).data.cpu().numpy()
         test_y_pred = softmax_output.argmax(axis=1)
-        cur_acc = (test_y_pred==test_y).sum() / len(test_y)
-        if train_iter%10 == 0:
+        cur_acc = (test_y_pred == test_y).sum() / len(test_y)
+        if train_iter % 10 == 0:
             print(train_iter, 'test acc:', cur_acc)
-        out_file.write('%f\n'%cur_acc)
-        if train_iter%50==0:
+        out_file.write('%f\n' % cur_acc)
+        if train_iter % 50 == 0:
             torch.save(enc.state_dict(),
-                       '%s/%i_encoder.model'%(ckpt_path, train_iter))
+                       '%s/%i_encoder.model' % (ckpt_path, train_iter))
             torch.save(predictor.state_dict(),
-                       '%s/%i_predictor.model'%(ckpt_path, train_iter))
+                       '%s/%i_predictor.model' % (ckpt_path, train_iter))
     out_file.close()
 
 
 if __name__ == '__main__':
-    features = ['v_enc_dim300', 'v_freq_freq', 'v_deg', 'v_sense', 'e_vertexsim',
-                'e_dir', 'e_rel', 'e_weightsource', 'e_srank_rel', 'e_trank_rel', 'e_sense']
+    features = [
+        'v_enc_dim300', 'v_freq_freq', 'v_deg', 'v_sense', 'e_vertexsim',
+        'e_dir', 'e_rel', 'e_weightsource', 'e_srank_rel', 'e_trank_rel',
+        'e_sense'
+    ]
     # features = ['v_deg', 'v_sense', 'e_weightsource', 'e_srank_rel']
     # heuristics = ['st', 'pairwise', 'rf', 'length']
     heuristics = []
-    train('science', features, heuristics, 20, 0.8, 'science_train.log', False, 100, 1000, 'science_ckpt')
-    # train('open_domain', features, 10, 0.95, 'open_domain_train.log', False, 12000, 100, 'open_domain_ckpt')
+    train('science', features, heuristics, 20, 0.8, 'science_train.log', False,
+          100, 1000, 'science_ckpt')
+    # train('open_domain', features, heuristics, 10, 0.95, 'open_domain_train.log', False, 100, 100, 'open_domain_ckpt')
